931bb049767b8368b83d23cfe21f1931c6127bb9
Ross Williams <rossjrw@gmail.com>
2020-05-27 17:38:22 +0100

    It finally renders something

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <form>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <form>
+          <p>hello!</p>
+          <hr>
 
-        <hr>
-
-      </form>
-    </div>
-  </section>
+        </form>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
+export default {
+  name: "App"
+}
 </script>
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import './root.scss';
 import Vue from "vue";
 import App from './App.vue';
 
-new Vue({
+let vm = new Vue({
   el: '#app',
   template: '<App/>',
-  components: {App}
+  components: { App },
+  render: h => h(App),
 })
-
-console.log("help")
