2b8df8e88df33266788970c396a7130407e4f875
Ross Williams <rossjrw@gmail.com>
2020-05-27 14:36:47 +0100

    Move the bulk of index.html to the main component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-light">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">Regex Series Replace</h1>
+        <h3 class="subtitle is-3">Make a series of regex find-and-replacements.</h3>
+      </div>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <form>
+
+        <hr>
+
+      </form>
+    </div>
+  </section>
+</template>
