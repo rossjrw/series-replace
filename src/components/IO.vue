547a831f7c685a07f1c4b67bfe8f1b4cda21a67e
Ross Williams <rossjrw@gmail.com>
2020-05-27 13:11:39 +0100

    Move chunks of the index to separate components

---
+++ src/components/IO.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="field is-horizontal">
+    <div class="field-body">
+      <div class="field">
+        <label class="label">Input</label>
+        <div class="control is-expanded">
+          <textarea class="textarea"
+                    placeholder="Input text"></textarea>
+        </div>
+        <p>Paste the text you want replaced here.</p>
+      </div>
+      <div class="field">
+        <label class="label">Output</label>
+        <div class="control is-expanded">
+          <textarea readonly
+                    class="textarea"
+                    placeholder="Output text"></textarea>
+        </div>
+        <p>The replaced text will appear here.</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Rule.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="field is-horizontal">
+    <div class="field-body">
+      <div class="field">
+        <label class="label">Find</label>
+        <div class="control is-expanded">
+          <input class="input"
+                 type="text"
+                 placeholder="Regular expression">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Replace</label>
+        <div class="control is-expanded">
+          <input class="input"
+                 type="text"
+                 placeholder="Replacement text">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
