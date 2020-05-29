13da7313febe87f0bca40acc2fd5425b30f2f718
Ross Williams <rossjrw@gmail.com>
2020-05-29 15:18:48 +0100

    Begin implementing Vuex

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import './root.scss';
-import Vue from "vue";
-import App from './App.vue';
+import './root.scss'
+import Vue from "vue"
+import Vuex from "vuex"
+import App from './App.vue'
+
+Vue.use(Vuex)
+
+let store = new Vuex.Store({
+  state: {
+    input_text: "",
+    output_text: "",
+    rules: [],
+  }
+})
 
 let vm = new Vue({
   el: '#app',
   template: '<App/>',
   components: { App },
   render: h => h(App),
+  store,
 })
