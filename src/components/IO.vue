78e2236f091a6c01cc58b2bb9c518d319fae2330
Ross Williams <rossjrw@gmail.com>
2020-05-29 19:26:54 +0100

    Move text to placeholder

--- src/components/IO.vue
+++ src/components/IO.vue
@@ -5,18 +5,18 @@
         <label class="label">Input</label>
         <div class="control is-expanded">
           <textarea class="textarea"
-                    placeholder="Input text"></textarea>
+                    placeholder="Paste the text you want replaced here.">
+          </textarea>
         </div>
-        <p>Paste the text you want replaced here.</p>
       </div>
       <div class="field">
         <label class="label">Output</label>
         <div class="control is-expanded">
           <textarea readonly
                     class="textarea"
-                    placeholder="Output text"></textarea>
+                    placeholder="The replaced text will appear here.">
+          </textarea>
         </div>
-        <p>The replaced text will appear here.</p>
       </div>
     </div>
   </div>
