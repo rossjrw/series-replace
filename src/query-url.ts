8ef15fca9f673a7cd4f2cbdbf4c037b0e1ec5a3d
Ross Williams <rossjrw@gmail.com>
2020-06-02 15:57:14 +0100

    Finish parsing URL

--- src/query-url.ts
+++ src/query-url.ts
@@ -1,12 +1,33 @@
-import { parse } from 'query-string'
-import pick from 'lodash/pick'
+import { parse } from "query-string"
+import { defaults, castArray, mapValues, pick, zip } from "lodash"
 
 import { Rule } from './types'
 
-export function getDataFromUrl(loc: string): [Rule[], string?] {
+interface ParsedUrl {
+  i: string[]
+  f: string[]
+  r: string[]
+}
+
+export function getDataFromUrl(loc: string): [Rule[], string] {
   // Get the data state from the URL
-  const parsed_url = pick(parse(loc), ["i", "f", "r"])
-  return [[], "hello!"]
+  let parsed: ParsedUrl = mapValues(
+    pick(parse(loc), ["i", "f", "r"]),
+    value => castArray(value)
+  )
+  defaults(parsed, {i: [""], f: [""], r: [""]})
+  const inputText: string = parsed.i.join("\n")
+  const rules: Rule[] = zip(parsed.f, parsed.r).map(
+    (fr, index) => {
+      return {
+        find: fr[0] || "",
+        replacement: fr[1] || "",
+        id: index
+      }
+    }
+  )
+  console.log(rules, inputText)
+  return [rules, inputText]
 }
 
 export function saveDataToUrl() {
