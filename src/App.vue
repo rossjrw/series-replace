342a6eeb2d319b8b3c3be90bd9a454900059743b
Ross Williams <rossjrw@gmail.com>
2020-06-05 01:10:35 +0100

    Create component to expose URL

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,23 @@
         </form>
       </div>
     </section>
+    <SaveRules></SaveRules>
     <Footer></Footer>
   </div>
 </template>
 
 <script lang="ts">
 import Corner from '@/components/Corner.vue'
-import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
 import InputOutput from '@/components/IO.vue'
 import Ruleset from '@/components/Ruleset.vue'
+import SaveRules from '@/components/SaveRules.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: "App",
   components: {
-    Corner, Footer, Header, InputOutput, Ruleset
+    Corner, Footer, Header, InputOutput, Ruleset, SaveRules
   },
 }
 </script>
---
+++ src/components/SaveRules.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <input class="input"
+             type="text"
+             :value="renderedUrl">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { saveDataToUrl } from '@/query-url'
+
+export default {
+  name: "SaveRules",
+  computed: {
+    renderedUrl: {
+      get(): string {
+        return saveDataToUrl(
+          this.$store.state.rules, this.$store.state.inputText
+        )
+      }
+    }
+  }
+}
+</script>
